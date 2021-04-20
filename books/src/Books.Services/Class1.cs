35b1882d68dbad50fc9575c40687c460d92abb61
Ignacio Pujols <ignaciopujols@gmail.com>
2021-04-20 11:35:44 -0400

    First Commit
    - App creation
    - Folder structure

---
+++ books/src/Books.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Books.Services
+{
+    public class Class1
+    {
+    }
+}
---
+++ books/src/Books.Utilities/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Books.Utilities
+{
+    public class Class1
+    {
+    }
+}
