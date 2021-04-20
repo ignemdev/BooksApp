d6fd157b0619fedc5c7ab4f21d9a85ef154207b9
Ignacio Pujols <ignaciopujols@gmail.com>
2021-04-20 12:25:59 -0400

    Book model
    -Book model implemented
    -WheatherForecast model deleted

--- books/src/Books.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Books.Core
-{
-    public class Class1
-    {
-    }
-}
--- books/src/Books.Utilities/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Books.Utilities
-{
-    public class Class1
-    {
-    }
-}
