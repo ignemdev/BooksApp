11c4baef60f491a075189d5192a40fc2eadbdcbe
Ignacio Pujols <ignaciopujols@gmail.com>
2021-04-20 11:54:37 -0400

    Cors policy
    Cross origin policy implemented to allow any origin

--- books/src/Books.Api/Startup.cs
+++ books/src/Books.Api/Startup.cs
@@ -26,7 +26,7 @@ namespace Books.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddCors();
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -47,6 +47,12 @@ namespace Books.Api
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .SetIsOriginAllowed(origin => true)
+            );
 
             app.UseAuthorization();
 
