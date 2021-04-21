5bc572a59d18c4ca544d1be24b7f254fce8a3e3b
Ignacio Pujols <ignaciopujols@gmail.com>
2021-04-20 20:55:56 -0400

    React template
    -React template implemented
    -React bootstrap installed

---
+++ book-frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Button from 'react-bootstrap/Button'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hola mundo klk</h1>
+      <Button variant="primary">Klk</Button>
+    </div>
+  );
+}
+
+export default App;
